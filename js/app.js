fd0f9a6bff3661a49bf0ccdce698267d9729159c
Shaabolot <shaabolotamatov@gmail.com>
2023-02-25 13:22:20 +0600

    Gramotey

--- js/app.js
+++ js/app.js
@@ -7,9 +7,3 @@ const closeNav = () => {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
-
-// const dropbtn = document.querySelector(".dropbtn")
-
-// dropbtn.addEventListener("click", () => {
-//     dropdown_content.style.display = "none";
-// })
